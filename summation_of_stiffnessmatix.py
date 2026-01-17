46a69a751ae1a27bce0314b5adc278ab96119f5d
naranartphuvongpa-droid <naranartphuvongpa@gmail.com>
2026-01-17 20:47:46 +0000

    Refactor summation_of_stiffnessmatrix with weighted average
    Updated summation_of_stiffnessmatrix to include length weights for averaging.

--- summation_of_stiffnessmatix.py
+++ summation_of_stiffnessmatix.py
@@ -3,13 +3,14 @@ from transformation import transformation
 from stiffness_matrix import stiffness_matrix
 def summation_of_stiffnessmatrix(Vf_warp,Vf_weft,Vf_binder):
     C_warp = stiffness_matrix()
-    C_binder, C_weft, _, _ = transformation()
+    C_binder, C_weft, length, _, _ = transformation()
     C_total = []
     for i in range(len(C_binder)):
         C_sum = Vf_warp*C_warp + Vf_weft*C_weft + Vf_binder*C_binder[i]
         C_total.append(C_sum)
-    C_total_avg = np.mean(C_total, axis=0)
+    weights = np.array(length) / np.sum(length)
+    C_total_avg = np.tensordot(weights, C_total, axes=(0,0))
     np.set_printoptions(precision=3, suppress=True)
     print(C_total_avg)
     return C_total_avg
-summation_of_stiffnessmatrix(0.5, 0.3, 0.2)
+summation_of_stiffnessmatrix(0.5, 0.3, 0.2)0.2)
