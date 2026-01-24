82142011d1dd06ab1c275c31f3942a8137de5b6f
naranartphuvongpa-droid <naranartphuvongpa@gmail.com>
2026-01-24 14:08:31 +0000

    Add files via upload

---
+++ resin_stiffness.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def matrix_stiffness_isotropic(E, nu):
+    lam = (nu * E) / ((1 + nu) * (1 - 2 * nu))
+    mu  = E / (2 * (1 + nu))
+
+    C = np.zeros((6, 6))
+    C[0:3, 0:3] = lam
+    np.fill_diagonal(C[0:3, 0:3], lam + 2*mu)
+
+    C[3, 3] = mu
+    C[4, 4] = mu
+    C[5, 5] = mu
+    return C
